3c4b6aef12aad62c6d120e0fd2f20f019b231876
Davide <93427056+DavideDiCaprio@users.noreply.github.com>
2023-01-17 11:49:04 +0100

    add throw_n_dices function

--- dices.py
+++ dices.py
@@ -1,8 +1,14 @@
 import random
 
+
 def throw_dice(n_faces=6):
-  
   outcome = random.randint(1,n_faces)
   return outcome
 
-throw_dice()
+
+def throw_n_dices(n_dices,n_faces=6):
+  list_of_outcomes = []
+  while len(list_of_outcomes) != n_dices:
+    list_of_outcomes.append(throw_dice(n_faces=6))
+
+  return list_of_outcomes
