5738638251d8978e22acc31583ed7dc567e0f97c
Davide Di Caprio <dicapriodavide@outlook.com>
2025-10-01 18:05:22 +0000

    create print_matrix and create_identity_matrix function

--- linear_algebra/matrix.py
+++ linear_algebra/matrix.py
@@ -1,1 +1,40 @@
+'''
+ matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+'''
 
+def print_matrix(matrix: list[list[int]]):
+    '''
+    print matrix
+    '''
+    print("[")
+    for row in matrix:
+        print(f"    {row},")
+    print("]")
+
+
+def create_identity_matrix(size: int) -> list[list[int]]: 
+    '''
+    create identity matrix of given size
+    '''
+    matrix = []
+    for i in range(size):
+        row = []
+
+        for j in range(size):
+            if i == j:
+                row.append(1)
+            else:
+                row.append(0)
+        
+        matrix.append(row)
+    
+    return matrix
+
+if __name__ == '__main__':
+    identity_4x4 = create_identity_matrix(4)
+    
+    print_matrix(identity_4x4)
